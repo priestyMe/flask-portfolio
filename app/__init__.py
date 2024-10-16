8d6907cf06d444edbc9734ea89fb8ba3db7efa42
Olanrewaju Oyekale <oyekaleop@gmail.com>
2024-10-17 02:08:53 +1030

    environment setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
+from flask.cli import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 
 # Create a database instance
@@ -6,7 +8,13 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://ptadmin:M0fOluwash9@ptfoliodemoserver.database.windows.net:1433/ptfdatabase?driver=ODBC+Driver+17+for+SQL+Server'
+
+    load_dotenv()  # Load environment variables from .env file
+
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mssql+pyodbc://{DB_USER}:{DB_PASSWORD}@ptfoliodemoserver.database.windows.net:1433/ptfdatabase?driver=ODBC+Driver+17+for+SQL+Server'
 
     # Initialize the app with the database
     db.init_app(app)
